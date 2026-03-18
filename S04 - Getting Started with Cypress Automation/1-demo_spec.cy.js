3135fe73a1573bb6c98a5afada94275369715f3a
zeashantayyab <zeashan.tayyab@gmail.com>
2026-03-18 23:46:55 +0400

    comment added

--- S04 - Getting Started with Cypress Automation/1-demo_spec.cy.js
+++ S04 - Getting Started with Cypress Automation/1-demo_spec.cy.js
@@ -1,9 +1,11 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
-    })
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(true);
+  });
 
-    it('Does not do much!', () => {
-        expect(true).to.equal(false)
-    })
-  })
+  it("Does not do much!", () => {
+    expect(true).to.equal(false);
+  });
+});
+
+//need to run npx cypress open in terminal to open the cypress test runner and then click on the demo_spec.cy.js file to run the tests.

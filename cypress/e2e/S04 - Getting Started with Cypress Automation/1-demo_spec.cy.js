fd478c37923e982791e289279f7716fc2d2ad1ec
zeashantayyab <zeashan.tayyab@gmail.com>
2026-03-20 02:32:49 +0400

    Cypress-repaired

---
+++ cypress/e2e/S04 - Getting Started with Cypress Automation/1-demo_spec.cy.js
@@ -0,0 +1,12 @@
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("Does not do much!", () => {
+    expect(true).to.equal(false);
+  });
+});
+
+//need to run npx cypress open in terminal to open the cypress test runner and then click on the demo_spec.cy.js file to run the tests.
+//new branch comment

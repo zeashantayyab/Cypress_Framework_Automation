d919ca53a49d73585f6707ed86f9badd3cc3d4bd
zeashantayyab <zeashan.tayyab@gmail.com>
2026-03-20 02:57:09 +0400

    changesPOM

--- S08 - Cypress Assertions/1-implicit_assertion_demo_spec.js
+++ S08 - Cypress Assertions/1-implicit_assertion_demo_spec.js
@@ -1,15 +1,17 @@
-describe('Implicit Assertion', () => {
-    it('should understand implicit assertions in cypress', () => {
-      cy.visit('https://courses.letskodeit.com/practice')
+describe("Implicit Assertion", () => {
+  it("should understand implicit assertions in cypress", () => {
+    cy.visit("https://courses.letskodeit.com/practice");
 
-      cy.get('#product')
-      .should('have.class', 'table-display')
-      .and('be.visible')
-      .find('tbody tr:nth-child(2)')
-      .find('td')
+    cy.get("#product")
+      .should("have.class", "table-display")
+      .and("be.visible")
+      .find("tbody tr:nth-child(2)")
+      .find("td")
       .last()
-      .should('contain', '35')
-      .and('have.text', '35')
-      .and('have.class', 'price')
-    })
-  })
+      .should("contain", "35")
+      .and("have.text", "35")
+      .and("have.class", "price");
+
+    //adding comments
+  });
+});

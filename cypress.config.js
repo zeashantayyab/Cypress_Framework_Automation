78cda17b2d7914574db862a15493310b83c2d653
zeashantayyab <zeashan.tayyab@gmail.com>
2026-03-21 09:05:18 +0400

    configuration

--- cypress.config.js
+++ cypress.config.js
@@ -3,40 +3,37 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "3t4yie",
   defaultCommandTimeout: 6000,
-  //reporter: "cypress-mochawesome-reporter",
+  reporter: "cypress-mochawesome-reporter",
   screenshotsFolder: "cypress/screenshots",
-  videosFolder: "cypress/videos", // This will specify the folder where Cypress saves videos of test runs.
-  allowCypressEnv: true, // This will prevent Cypress from automatically adding environment variables to the test runner UI, which can help keep sensitive information secure.
+  videosFolder: "cypress/videos",
+  allowCypressEnv: true,
 
   env: {
-    username: "adeel8800",
+    username: "adeel18800",
     password: "Smart786!",
-    baseUrl: "https://training.worldlogisticspassport.com",
   },
 
   e2e: {
     viewportWidth: 1920,
     viewportHeight: 1080,
     screenshotOnRunFailure: true,
-    videosFolder: "cypress/videos",
-    videoOnFailure: true,
     video: false,
     baseUrl: "https://training.worldlogisticspassport.com",
 
-    /* setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
-    },*/
+      return config;
+    },
   },
-  //reporterOptions: {
-  //  reportDir: "cypress/reports",
-  //  charts: true,
-  //  reportPageTitle: "Test Report",
-  //  embeddedScreenshots: true,
-  //  inlineAssets: true,
-  override: false,
-  videoOnFail: true,
-  saveAllAttempts: false,
-  timeStamp: "longDate",
-  //},
+
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    reportPageTitle: "Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: false,
+    saveAllAttempts: false,
+    timestamp: false,
+  },
 });
